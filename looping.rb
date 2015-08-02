08e5cc6bd6fc749309b16700a8a9d626e9e8b0e4
rolandobrown <rolando.brown@gmail.com>
2015-08-02 19:19:27 -0400

    loop Wingardium Leviosa

--- looping.rb
+++ looping.rb
@@ -1,5 +1,27 @@
 def looping
-	#your code here
+  loop do
+    puts "Wingardium Leviosa"
+  end
 end
 
+looping
+
 #call your method here
+
+## DONE
+
+# 1. Fork and clone this lab. 
+
+# PENDING
+
+# It's your first year at Hogwarts and you're having a 
+# tough time getting the hang of that levitation spell. 
+# Fill out the content of the method `looping` to contain a `loop` 
+# that `puts` the levitation charm `"Wingardium Leviosa"` 
+# an infinite number of times. 
+# Beneath your method definition, 
+# call your method by typing the name of the method, `looping`.
+# 3. Then, run the file by typing `ruby looping.rb` 
+# in your terminal from the directory of this lab. 
+# You'll be stuck in an infinite levitation loop! 
+# Hit `Control`+`C` to exit the infinite loop. 
